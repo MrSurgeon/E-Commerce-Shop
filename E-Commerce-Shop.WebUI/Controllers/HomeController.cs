b4a07c1a09f3234620e3d1a6a7018cf6716c0616
Enes Cerrah <enescerrah1995@gmail.com>
2021-07-24 13:53:28 +0300

    Integration with MVC pattern
    Created first routemap and for example added two Controller Class.

---
+++ E-Commerce-Shop.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+namespace E_Commerce_Shop.WebUI.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Home/Index";
+        }
+        public string About()
+        {
+            return "Home/About";
+        }
+    }
+}
---
+++ E-Commerce-Shop.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+namespace E_Commerce_Shop.WebUI.Controllers
+{
+    public class ProductController
+    {
+        public string List()
+        {
+            return "Product/List";
+        }
+        public string Details()
+        {
+            return "Product/Details";
+        }
+    }
+}
