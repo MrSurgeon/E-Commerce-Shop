fad08600e42df6a67346d7d06496dc3ab78deabb
Enes Cerrah <enescerrah1995@gmail.com>
2021-07-25 16:47:19 +0300

    Adding a Layout Page and Creating Sections

--- E-Commerce-Shop.WebUI/Controllers/HomeController.cs
+++ E-Commerce-Shop.WebUI/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 
 
 using System;
+using System.Collections.Generic;
+using E_Commerce_Shop.WebUI.Models;
+using E_Commerce_Shop.WebUI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Commerce_Shop.WebUI.Controllers
@@ -9,9 +12,19 @@ namespace E_Commerce_Shop.WebUI.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.Greeting = (DateTime.Now.Hour) > 12 ? "İyi Günler" : "Günaydın";
-            ViewBag.Name = "Enes";
-            return View();
+            var productViewModel = new ProductViewModel()
+            {
+                Products = new List<Product>()
+                {
+                    new Product(){Name="Samsung S3",Price=1000,Description="Smart Phone"},
+                    new Product(){Name="Samsung S4",Price=2000,Description="Smart Phone",IsApproved=true},
+                    new Product(){Name="Samsung S5",Price=3000,Description="Smart Phone"},
+                    new Product(){Name="Samsung S6",Price=4000,Description="Smart Phone",IsApproved=true},
+                    new Product(){Name="Samsung S7",Price=5000,Description="Smart Phone"},
+                    new Product(){Name="Samsung S8",Price=6000,Description="Smart Phone",IsApproved=true},
+                }
+            };
+            return View(productViewModel);
         }
         public IActionResult About()
         {
